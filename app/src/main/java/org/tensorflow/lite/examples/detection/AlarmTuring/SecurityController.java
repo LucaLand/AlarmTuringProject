eb5e4b627611dc1e4808a70d934f57f7cb303ae0
LucaLand <luca.landolfi3@studio.unibo.it>
2021-03-18 20:44:29 +0100

    Writed more optimal and readable code + projected and fixed time controlling detections but not implemented

--- app/src/main/java/org/tensorflow/lite/examples/detection/AlarmTuring/SecurityController.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/AlarmTuring/SecurityController.java
@@ -2,20 +2,28 @@ package org.tensorflow.lite.examples.detection.AlarmTuring;
 
 
 
-import android.text.format.Time;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
 
 import org.tensorflow.lite.examples.detection.AlarmTuring.Alerts.Alert;
 import org.tensorflow.lite.examples.detection.AlarmTuring.Alerts.AlertFactory;
 import org.tensorflow.lite.examples.detection.AlarmTuring.DetectionUtils.CategoryFilterFactory;
+import org.tensorflow.lite.examples.detection.AlarmTuring.DetectionUtils.DetectionCategoryType;
 import org.tensorflow.lite.examples.detection.tflite.Detector;
 
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Objects;
+
+
 
 public class SecurityController {
 
+    private final int RESET_TIMEOUT_SECONDS = 10;
+
     private final List<RelationCategoryToAlert> relationList;
-    Timestamp timestamp[];
+
 
     public static SecurityController createSecurityController(SecurityLevel securityLevel) {
         return new SecurityController(securityLevel);
@@ -23,7 +31,9 @@ public class SecurityController {
 
     public SecurityController(SecurityLevel securityLevel) {
         this.relationList = securityLevel.getRel();
+
     }
+
 
     public void run(List<Detector.Recognition> detectionList){
         /** La check deve fare tutto;
@@ -36,32 +46,44 @@ public class SecurityController {
         }
     }
 
+
     private void checker(List<Detector.Recognition> detectionList, RelationCategoryToAlert rel, int nRel){
-        Alert alert = AlertFactory.createAlert(rel.getAlertType());
-        int num = 0;
+        /** Creating the alert that handles the specific Alerting detection*/
+        Alert alertType = AlertFactory.createAlert(rel.getAlertType());
+
+        int num = 0, minOccurrences = rel.getMinObjectNumber();
         int time = rel.getTimeSeconds();
-        for(Detector.Recognition detection : detectionList){
-            if(CategoryFilterFactory.createSimpleFilter(rel.getDetectionCategory()).categoryCheck(detection.getTitle())) {
+
+
+        for(Detector.Recognition detection : detectionList) {
+            if(checkCategory(detection, rel.getDetectionCategory())){
                 num++;
-                if (num >= rel.getMinObjectNumber()) {
-                    if (timestamp[nRel].equals(null))
-                        timestamp[nRel] = new Timestamp(System.currentTimeMillis());
-
-                    if (rel.isDECREASE_TIME_BY_NUMBER())
-                        time = time/num;
-
-                    if (timestamp[nRel].compareTo(new Timestamp(System.currentTimeMillis())) > time)
-                        alert.alert();
-
-                }else{
-                    timestamp[nRel] = null;
+                if(minOccurencesCheck(num, minOccurrences)){
+                    if(timeCheck(nRel))
+                        alertType.alert();
                 }
-            }else{
-                timestamp[nRel] = null;
             }
 
-
         }
+
+
     }
 
+    private boolean checkCategory(Detector.Recognition detection, DetectionCategoryType categoryType ){
+        return CategoryFilterFactory.createSimpleFilter(categoryType).check(detection);
+    }
+
+    private boolean minOccurencesCheck(int occ, int minOcc){
+        if(occ >= minOcc)
+            return true;
+        return false;
+    }
+
+    private boolean timeCheck(int numRel){
+        /**TODO. Il time check
+         * In modo tale che la detection di un tale oggetto incrementi un contatore(level of alert) che rappresenta
+         * i secondi di detection dell'elemento, ogni secondo nel quale avviene almeno una detection, e questo si azzeri
+         * in caso passi un tempo standard (COSTANT) tipo 10s*/
+        return true;
+    }
 }
