dfffe5326b801adf1ffeba5dc5e74a0a9e108b8a
LucaLand <luca.landolfi3@studio.unibo.it>
2021-03-18 20:43:11 +0100

    Added initDetectionFilters factory method

--- app/src/main/java/org/tensorflow/lite/examples/detection/AlarmTuring/DetectionUtils/CategoryFilterFactory.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/AlarmTuring/DetectionUtils/CategoryFilterFactory.java
@@ -1,5 +1,9 @@
 package org.tensorflow.lite.examples.detection.AlarmTuring.DetectionUtils;
 
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+
 public class CategoryFilterFactory {
 
     public static CategoryFilter createSimpleFilter(DetectionCategoryType categoryType){
@@ -14,4 +18,42 @@ public class CategoryFilterFactory {
                 return null;
         }
     }
+
+
+
+    public static CategoryFilter createRecognitionFilter(DetectionCategoryType categoryType){
+        switch (categoryType){
+            case ANIMAL:
+                return new CategoryFilter("dog,cat,horse,bear,bird,cow;", "Animal");
+            case PERSON:
+                return new CategoryFilter("person;", "Person");
+            case VEHICLE:
+                return new CategoryFilter("car,truck,bicycle,motorcycle;", "Vehicle");
+            default:
+                throw new EnumConstantNotPresentException(DetectionCategoryType.class, categoryType.toString());
+        }
+    }
+
+    public static List<CategoryFilter> initializeRecognitionFilters(){
+        List<CategoryFilter> filterList = new LinkedList<>();
+
+        try{
+            return CategoryFilterFactory.initializeRecognitionFiltersFromFile();
+        }catch (FileNotFoundException e) {
+            filterList.add(CategoryFilterFactory.createRecognitionFilter(DetectionCategoryType.PERSON));
+            filterList.add(CategoryFilterFactory.createRecognitionFilter(DetectionCategoryType.ANIMAL));
+            filterList.add(CategoryFilterFactory.createRecognitionFilter(DetectionCategoryType.VEHICLE));
+        }
+
+        return filterList;
+    }
+
+
+    protected static List<CategoryFilter> initializeRecognitionFiltersFromFile() throws FileNotFoundException {
+        /** TODO implement file read for category selection ("Animal: dog,cat,horse,etc;\n) */
+        throw new FileNotFoundException();
+    }
+
 }
+
+
