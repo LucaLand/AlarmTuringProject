8b2512b506dbcf2559d0376266b44df980262dd6
LucaLand <luca.landolfi3@studio.unibo.it>
2021-03-19 13:17:50 +0100

    Added personalized Alarm-Turing Logger

---
+++ app/src/main/java/org/tensorflow/lite/examples/detection/AlarmTuring/Logger.java
@@ -0,0 +1,7 @@
+package org.tensorflow.lite.examples.detection.AlarmTuring;
+
+public class Logger {
+    public static void write(String message){
+        System.out.println("--ALARM-TURING-- log:  " + message);
+    }
+}
