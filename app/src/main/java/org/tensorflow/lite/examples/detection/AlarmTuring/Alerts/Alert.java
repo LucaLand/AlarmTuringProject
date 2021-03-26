ec4ab731b15b14f359532b58543ec8d43bab2b06
LucaLand <luca.landolfi3@studio.unibo.it>
2021-03-26 11:20:17 +0100

    Transformed into Abstract class extending Thread Implemented Alert.java as a parallel thread to simulate activation.

--- app/src/main/java/org/tensorflow/lite/examples/detection/AlarmTuring/Alerts/Alert.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/AlarmTuring/Alerts/Alert.java
@@ -1,5 +1,44 @@
 package org.tensorflow.lite.examples.detection.AlarmTuring.Alerts;
 
-public interface Alert {
-    void alert();
+import org.tensorflow.lite.examples.detection.AlarmTuring.Logger;
+
+public abstract class Alert extends Thread{
+
+    private boolean engaged = false;
+
+
+    @Override
+    public void run() {
+            alarmAlert();
+    }
+
+
+
+    public void alert(){
+        if(!engaged) {
+            engaged = true;
+            Logger.writeDebug("THREAD Created || NThread:" + Thread.activeCount());
+            this.start();
+        }
+        Logger.write(this.getClass().toString() + ": ENGAGED!");
+    }
+
+    public void reset(){
+        engaged=false;
+    }
+
+    public boolean isEngaged() {
+        return engaged;
+    }
+
+    private void setEngaged(boolean engaged) {
+        this.engaged = engaged;
+    }
+
+    /**
+     * This function will be execute by a dedicated Thread
+     * Remember to use Thread.sleep() decrease the execution Frequency
+     */
+    public abstract void alarmAlert();
+
 }
