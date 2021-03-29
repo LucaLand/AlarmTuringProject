99fad7b57045c5a51d7292de53f12baf68df5c4a
LucaLand <luca.landolfi3@studio.unibo.it>
2021-03-29 16:56:57 +0200

    Modified SecurityLevels preset.

--- app/src/main/java/org/tensorflow/lite/examples/detection/AlarmTuring/SecurityLevelsUtils/SecurityLevelEnum.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/AlarmTuring/SecurityLevelsUtils/SecurityLevelEnum.java
@@ -10,12 +10,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum SecurityLevelEnum implements SecurityLevel {
-    MAXIMUM(6,"Sicurezza: Massima", "PERSON,SOUND,6,2,true; ANIMAL,MAIL,8,2,false; VEHICLE,MSG,3,1,true"),
-    FUORICASA(5,"Sicurezza:Alto Livello - Fuori casa","PERSON,SOUND,5,1,true; ANIMAL,MAIL,8,2,false;"),
-    HIGH(4,"Sicurezza:Alto Livello - Fuori casa", "PERSON,SOUND,5,1,true; ANIMAL,MAIL,8,2,false;"),
-    MEDIUM(3,"Sicurezza: media", "PERSON,SOUND,5,1,true; ANIMAL,MAIL,8,2,false;"),
-    INCASA(2,"Sicurezza: bassa - in casa", "PERSON,SOUND,8,1,true;"),
-    LOW(1, "Sicurezza bassa", Arrays.asList(new RelationCategoryToAlert(DetectionCategoryType.PERSON,AlertType.MSG)), "Descrizione");
+    MAXIMUM(6,"Sicurezza: Massima", "PERSON,SOUND,6,2,true; ANIMAL,SOUND,8,2,false; VEHICLE,SOUND,3,1,true"),
+    FUORICASA(5,"Sicurezza:Alto Livello - Fuori casa","PERSON,SOUND,4,1,true; ANIMAL,MAIL,6,2,false; VEHICLE,MAIL,4,2,false;"),
+    HIGH(4,"Sicurezza:Alto Livello - Fuori casa", "PERSON,SOUND,5,1,true; ANIMAL,MAIL,6,2,false; VEHICLE,MSG,5,1,false;"),
+    MEDIUM(3,"Sicurezza: media", "PERSON,SOUND,8,1,true; ANIMAL,MAIL,8,2,false;"),
+    INCASA(2,"Sicurezza: bassa - in casa", "PERSON,SOUND,8,2,true;"),
+    LOW(1, "Sicurezza bassa", "PERSON,MSG,8,1,false;", "Descrizione");
 
 
     /**     PARAMETERS      */
@@ -31,13 +31,21 @@ public enum SecurityLevelEnum implements SecurityLevel {
         this.rel = RelationFactory.createRelationList(relationString);
     }
 
+    SecurityLevelEnum(int lv, String levelName, String relationString, String desc) {
+        this.lv = lv;
+        this.nomeLivello = levelName;
+        this.rel = RelationFactory.createRelationList(relationString);
+        this.descrizione = desc;
+    }
+
+    @Deprecated
     SecurityLevelEnum(int lv, String levelName, List<RelationCategoryToAlert> rel, String desc) {
         this.lv = lv;
         this.nomeLivello = levelName;
         this.descrizione = desc;
         this.rel = rel;
     }
-
+    @Deprecated
     SecurityLevelEnum(int lv, String levelName, List<RelationCategoryToAlert> rel) {
         this.lv = lv;
         this.nomeLivello = levelName;
